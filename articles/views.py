96fc7ef90f5612214aa3eb83c5832af8c2992005
SIRAfrasiyab <ahmadreza.afrasiabie@gmail.com>
2025-06-03 13:52:32 +0330

    details compleated

--- articles/views.py
+++ articles/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 from . import models
 # Create your views here.
 def articles_list(request):
     articles = models.Articles.objects.all().order_by("date")
     args = {"articles":articles}
     return render(request, 'articles/articleslist.html', args)
+
+def articles_slug(request, slug):
+    # return HttpResponse(slug)
+    article = models.Articles.objects.get(slug=slug)
+    return render(request, 'articles/article_details.html', {'article':article})
